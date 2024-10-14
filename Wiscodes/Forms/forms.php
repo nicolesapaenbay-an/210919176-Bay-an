bf1d99e966f7e955057141c54729b45d8a16f7cb
Nicole <kolassang05>
2024-10-14 16:26:06 +0800

    Edit the Code in Forms Folder - forrms.php

--- Wiscodes/Forms/forms.php
+++ Wiscodes/Forms/forms.php
@@ -3,39 +3,36 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Inputs</title>
+    <title>PHP Input Example</title>
 </head>
 <body>
-    <form method="post" action ="/forms/forms.php ">
-
-        <label for="name">Enter Your First Name:</label><br>
-                <input type="text" id "name" name="name">
-                <br>
-
-                <label for="lastname">Enter Your Last Name:</label><br>
-
-                <input type="text" id="lastname" name="lastname">              
-                <br>           <label for="age">Enter your Age:</label><br>
-                <input type="number" id="age" name="edad">
-                <br>
-                <br>
-                <input type="submit" value"Submit>
     
-    
-        </form>
+    <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+        <label for="fname">Enter Your First Name:</label><br>
+        <input type="text" id="fname" name="fname"><br><br>
+
+        <label for="lname">Enter Your Last Name:</label><br>
+        <input type="text" id="lname" name="lname"><br><br>
+
+        <label for="age">Enter Your Age:</label><br>
+        <input type="number" id="age" name="age"><br><br>
+        
+        <input type="submit" value="Submit">
+    </form>
+
     <?php
-        if ($_SERVER["REQUEST_METHOD"] == "POST"  ){
-            $name=htmlspecialchars($_POST['name']);
-            $age=htmlspecialchars($_POST['edad']);
-            $lastname=htmlspecialchars($_POST['lastname']);
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Capture the input from the form
+        $fname = htmlspecialchars($_POST['fname']);  // Sanitize user input
+        $lname = htmlspecialchars($_POST['lname']);
+        $age = htmlspecialchars($_POST['age']);
 
-            echo "<h3>You Inputs</h3>";
-            echo "Name: " .$name ."<br>";
-            echo "Last Name: " .$lastname ."<br>";
-            echo "Age: " .$age ."<br>";
-            
-        };
-
+        // Display the captured input
+        echo "<h2>Your Input:</h2>";
+        echo "First Name: " . $fname . "<br>";
+        echo "Last Name: " . $lname . "<br>";
+        echo "Age: " . $age . "<br>";
+    }
     ?>
 
 </body>
